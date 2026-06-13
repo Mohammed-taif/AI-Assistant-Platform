8e8ddc8e54b518f8b2c99b58eb3c18dc02aaa228
Mohammed taif <taif4226@gmail.com>
2026-06-13 12:15:45 +0530

    added vercel url to cors

--- backend/main.py
+++ backend/main.py
@@ -61,7 +61,11 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://ai-assistant-platform-h6uoosqeq-mohamed-taif-s-projects.vercel.app",
+        "*"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
